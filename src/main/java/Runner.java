d631cb03ded16a3387e63363537afdfc066885a8
Ricardo Ruiz <rfpimpao@gmail.com>
2018-08-14 16:15:43 +0100

    C & R of CRUD done, testing U

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,44 @@
+import db.DBHelper;
+import models.Director;
+import models.Film;
+
+import java.util.List;
+
 public class Runner {
 
     public static void main(String[] args) {
+
+
+
+        Director quentin = new Director("Quentin Tarantino");
+        DBHelper.save(quentin);
+        Director kubrick = new Director("Stanley Kubrick");
+        DBHelper.save(kubrick);
+
+
+
+        Film pulp = new Film("Pulp Fiction", 10, quentin);
+        DBHelper.save(pulp);
+
+        Film basterds = new Film("Inglorius Basterds", 21, quentin);
+        DBHelper.save(basterds);
+
+        Film shining = new Film("The Shining,", 7, kubrick);
+        DBHelper.save(shining);
+
+        Film orange = new Film("The Mechanical Orange", 18, kubrick);
+        DBHelper.save(orange);
+
+        List<Film> films = DBHelper.getAll(Film.class);
+        List<Director> directors = DBHelper.getAll(Director.class);
+
+        Film foundFilm = DBHelper.findById(3, Film.class);
+
+        foundFilm.setAgeRating(105);
+        DBHelper.update(foundFilm);
+//
+//        Film anotherFoundFilm = DBHelper.findById(2, Film.class);
+//        DBHelper.delete(anotherFoundFilm);
 
     }
 }
--- src/main/java/models/Film.java
+++ src/main/java/models/Film.java
@@ -1,4 +1,60 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "films")
 public class Film {
+    String title;
+    int ageRating;
+    int id;
+    Director director;
+
+    public Film(String title, int ageRating, Director director) {
+        this.title = title;
+        this.ageRating = ageRating;
+        this.director = director;
+    }
+
+    public Film() {
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "age_rating")
+    public int getAgeRating() {
+        return ageRating;
+    }
+
+    public void setAgeRating(int ageRating) {
+        this.ageRating = ageRating;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "director_id", nullable = false)
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
 }
